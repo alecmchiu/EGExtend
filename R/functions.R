8c23b3d08858b3c718aa1546e8b41489178cad08
Alec Chiu <alecmchiu@gmail.com>
2022-07-22 15:18:52 -0400

    Added license

--- R/functions.R
+++ R/functions.R
@@ -11,8 +11,8 @@ demean <- function(mat,class_df){
 generate_all_interaction <- function(mat) {
   dat <- mat
   colnames(dat) <- 1:ncol(dat)
-  do.call(cbind, combn(colnames(dat), 2, FUN= function(x)
-    list(setNames(data.frame(dat[,x[1]]*dat[,x[2]]),
+  do.call(cbind, utils::combn(colnames(dat), 2, FUN= function(x)
+    list(stats::setNames(data.frame(dat[,x[1]]*dat[,x[2]]),
                   paste(x, collapse="_")) )))
 }
 
@@ -20,9 +20,9 @@ generate_squared_terms <- function(g){
   return(apply(g,2,function(x){x^2}))
 }
 
-CILP <- function(prod_mat){
+CILP <- function(prod_mat,groupings){
     pvals <- apply(prod_mat,2,function(x){
-    ctest <- cor.test(scale(x),groupings)
+    ctest <- stats::cor.test(scale(x),groupings)
     return(c(ctest$estimate,ctest$p.value))
     })
 }
@@ -31,7 +31,7 @@ calculateEigengene <- function(mat){
   eigengene <- irlba::irlba(scale(mat),nv=1)$u
   rownames(eigengene) <- rownames(mat)
   avg_expr <- rowMeans(mat)
-  if (cor(avg_expr,eigengene[,1]) < 0){
+  if (stats::cor(avg_expr,eigengene[,1]) < 0){
     eigengene[,1] <- -eigengene[,1]
   }
   return(eigengene)
