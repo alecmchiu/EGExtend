1e7e4ca0b9094a18df54cd36dec23b75bac1c72f
Alec Chiu <alecmchiu@gmail.com>
2022-07-22 15:09:36 -0400

    Initial commit

--- R/functions.R
+++ R/functions.R
@@ -20,4 +20,19 @@ generate_squared_terms <- function(g){
   return(apply(g,2,function(x){x^2}))
 }
 
+CILP <- function(prod_mat){
+    pvals <- apply(prod_mat,2,function(x){
+    ctest <- cor.test(scale(x),groupings)
+    return(c(ctest$estimate,ctest$p.value))
+    })
+}
 
+calculateEigengene <- function(mat){
+  eigengene <- irlba::irlba(scale(mat),nv=1)$u
+  rownames(eigengene) <- rownames(mat)
+  avg_expr <- rowMeans(mat)
+  if (cor(avg_expr,eigengene[,1]) < 0){
+    eigengene[,1] <- -eigengene[,1]
+  }
+  return(eigengene)
+}
